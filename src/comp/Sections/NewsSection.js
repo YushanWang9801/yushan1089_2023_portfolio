78474eeb46366ca395cd57d958f20714c276d792
Yushan Wang <yushanwang0816@gmail.com>
2023-02-26 08:36:55 +0800

    mainpage gallery random done

--- src/comp/Sections/NewsSection.js
+++ src/comp/Sections/NewsSection.js
@@ -6,29 +6,17 @@ import projects from "./../Projects/projects.json";
 import useFirestore from '../../hooks/useFirestore';
 
 const news = blogs.slice(1, 3);
-const cards = projects.slice(0, 4);
+const cards = projects.slice(0, 3);
 
 const NewsSection = () => {
 
     const {docs} = useFirestore('images', "All");
-    const images = docs.slice(40, 45);
+    const shuffled = docs.sort(() => 0.5 - Math.random());
+    const images = shuffled.slice(0,5);
 
     return (
         <div className="newsSection">
             <div class="wrapper">
-                {/* <h2><strong>Latest Posts<span>( 4 )</span></strong></h2>
-
-                <div class="cards">
-                    {   
-                        cards.map(card => (
-                    <figure class="card">
-                        <img src={card.coverImage} />
-                        <figcaption>{card.name}</figcaption>
-                    </figure>
-                        ))
-                    }
-                </div> */}
-
                 <h2><strong>Latest Posts</strong></h2>
 
                 <div class="news">
@@ -44,6 +32,17 @@ const NewsSection = () => {
                         ))
                     }
                 </div>
+                <h2><strong>Featured Projects<span>( 3 )</span></strong></h2>
+                    <div class="cards">
+                        {   
+                            cards.map(card => (
+                        <figure class="card">
+                            <img src={card.coverImage} />
+                            <figcaption>{card.name}</figcaption>
+                        </figure>
+                            ))
+                        }
+                    </div>
 
                 <h2><strong>Recent Gallery Updates</strong></h2>
                 <div className="recent-gallery">
